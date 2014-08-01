4aa240b7695850cf495ed01d996e0f7bf83616f4
Manuel <elvictorfiee@gmail.com>
2014-08-01 14:55:46 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Manuel on 8/1/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
